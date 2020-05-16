9fd555244d0935f8810e91dd29253b4440f4f60b
fuzious <arpitsrivastava2012@gmail.com>
2020-05-16 19:21:36 +0530

    notLeapIfDivisibleBy100not400 test added

--- src/main/java/Leap/Leap.java
+++ src/main/java/Leap/Leap.java
@@ -7,4 +7,11 @@ public class Leap {
         else
             return false;
     }
+
+    public boolean notLeapIfDivisibleBy100not400(int year) {
+        if (year%100==0&&year%400==0)
+            return true;
+        else
+            return false;
+    }
 }
--- src/test/java/Leap/LeapTest.java
+++ src/test/java/Leap/LeapTest.java
@@ -11,4 +11,12 @@ class LeapTest {
         assertTrue(new Leap().leapIfDivisibleBy400(1600));
         assertFalse(new Leap().leapIfDivisibleBy400(1601));
     }
+
+    @Test
+    void notLeapIfDivisibleBy100not400() {
+        assertTrue(new Leap().notLeapIfDivisibleBy100not400(2000));
+        assertFalse(new Leap().notLeapIfDivisibleBy100not400(1700));
+    }
+
+
 }
